eaaa789955e562c9c53dc7b1a93a286ed28f13b0
oterpater <oteropaternina.n@gmail.com>
2020-05-25 17:53:29 -0500

    updating

--- frontend/src/pages/Preguntas.js
+++ frontend/src/pages/Preguntas.js
@@ -1,42 +1,62 @@
 import React from 'react';
-import { faLemon } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button } from 'react-bootstrap';
 
 import './Page.css';
 import Header from '../components/Header';
 import Aside from '../components/Aside';
 import Footer from '../components/Footer';
+import Survey from '../components/Encuesta';
 
-class Preguntas extends React.Component {
-  render() {
-    const info = {
+function Preguntas() {
+  const [ company, setCompany ] = React.useState("");
+
+  const info = {
       title: "PREGUNTAS DIAGNÓSTICO",
       module: "Módulo 01",
       moduleName: "Vigilancia Tecnológica",
     };
 
-    const pages = {
-      currentPage: 3,
-      previousPage: "/inteligencia",
-      nextPage: "/proceso"
-    };
+  const pages = {
+    currentPage: 3,
+    previousPage: "/inteligencia",
+    nextPage: "/proceso"
+  };
 
-    return (
-      <div className="page">
-        <Header module={info.module} title={info.title} moduleName={info.moduleName}/>
-        <Aside />
+  const handleChange = e => {
+    const { value } = e.target;
+    setCompany(value);
+  };
 
-        <main className="main">
-          <div className="content">
-            
-          </div>
-        </main>
+  return (
+    <div className="page">
+      <Header module={info.module} title={info.title} moduleName={info.moduleName}/>
+      <Aside />
 
-        <Footer pages={pages}/>
-      </div>
-    );
-  }
+      <main className="main">
+        <div className="content--questions">
+          <p>
+            Bloques de preguntas que se convierten en cuatro ejes: <strong>propósito, herramienta, procesos, estructura.</strong>
+          </p>
+          <form>
+            <label htmlFor="name" className="label">
+              Nombre de la empresa:
+              <input 
+                type="text"
+                name="name"
+                id="name"
+                value={company}
+                onChange={handleChange}
+                className="input"
+              />
+            </label>
+          </form>  
+          <Survey/>        
+        </div>
+      </main>
+
+      <Footer pages={pages}/>
+    </div>
+  );
 }
 
 export default Preguntas;
